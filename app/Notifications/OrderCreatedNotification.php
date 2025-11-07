05c219817216df8a6e5e88c154a0bc2639bb948d
Mahmoud Eltwansy <mahmoudtwansy1999@gmail.com>
2025-11-07 15:51:44 +0200

    Implement Broadcast Notification on OrderCreated Event using Pusher

--- app/Notifications/OrderCreatedNotification.php
+++ app/Notifications/OrderCreatedNotification.php
@@ -5,6 +5,7 @@ namespace App\Notifications;
 use App\Models\Order;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -13,12 +14,14 @@ class OrderCreatedNotification extends Notification
     use Queueable;
 
     protected $order;
+    protected $addr;
     /**
      * Create a new notification instance.
      */
     public function __construct(Order $order)
     {
         $this->order = $order;
+        $this->addr = $this->order->billingAddress;
     }
 
     /**
@@ -28,7 +31,7 @@ class OrderCreatedNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail', 'database'];
+        return ['mail', 'database', 'broadcast'];
     }
 
     /**
@@ -36,24 +39,32 @@ class OrderCreatedNotification extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
-        $addr = $this->order->billingAddress;
         return (new MailMessage)
             ->subject("New Order #{$this->order->number} Created.")
             ->greeting("Hi,{$notifiable->name},")
-            ->line("A new order (#{$this->order->number}) created by {$addr->name} from {$addr->country_name}")
+            ->line("A new order (#{$this->order->number}) created by {$this->addr->name} from {$this->addr->country_name}")
             ->action('View Order', url('/dashboard'))
             ->line('Thank you for using our application!');
     }
 
     public function toDatabase(object $notifiable)
     {
-        $addr = $this->order->billingAddress;
         return [
-            'body' => "A new order (#{$this->order->number}) created by {$addr->name} from {$addr->country_name}",
+            'body' => "A new order (#{$this->order->number}) created by {$this->addr->name} from {$this->addr->country_name}",
             'icon' => 'fas fa-file',
             'url' => url('/dashboard'),
             'order_id' => $this->order->id
         ];
+    }
+
+    public function toBroadcast(object $notifiable)
+    {
+        return new BroadcastMessage([
+            'body' => "A new order (#{$this->order->number}) created by {$this->addr->name} from {$this->addr->country_name}",
+            'icon' => 'fas fa-file',
+            'url' => url('/dashboard'),
+            'order_id' => $this->order->id
+        ]);
     }
 
     /**
